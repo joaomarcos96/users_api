df4cfd7fbded83d169657120b41cb3cd7f0faffb
Joao Marcos <jao.markos@hotmail.com>
2020-12-07 13:21:40 -0300

    Setup user policy

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,37 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(id: user.id)
+      end
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    admin_or_same_user?
+  end
+
+  def update?
+    admin_or_same_user?
+  end
+
+  def destroy?
+    admin_or_same_user?
+  end
+
+  private
+
+    def same_user?
+      record == user
+    end
+
+    def admin_or_same_user?
+      user.admin? || same_user?
+    end
+end
