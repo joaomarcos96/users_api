3ac8a92293db0e5517d6489c21b8fe5f93e6abd1
Joao Marcos <jao.markos@hotmail.com>
2020-12-08 20:29:44 -0300

    Validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@
 class User < ActiveRecord::Base
   extend Devise::Models
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
   include Discard::Model
+
+  validates :first_name, :last_name, :email, presence: true
 
   def active_for_authentication?
     super && !discarded?
