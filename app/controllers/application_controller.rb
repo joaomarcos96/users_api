cea77de5ae5b7f2151d9c43ffbb48e66d29ef664
Joao Marcos <jao.markos@hotmail.com>
2020-12-09 01:48:06 -0300

    Create rescue_from when record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,17 @@ class ApplicationController < ActionController::API
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    end
+
+  private
+
+    def record_not_found(error)
+      render json: { error: error.message }, status: :not_found
+    end
 end
